3894c1b2928a92c86b30ea0c6252bc43e2d102e0
AkhilaSriManasa <akhilasrimanasa7@gmail.com>
2020-05-28 04:07:29 +0530

    pr dat

--- test.py
+++ test.py
@@ -20,14 +20,14 @@ for file in f_list:
 
 	# # allconsFile = open("allcons.txt",'a',encoding="utf-8-sig", errors='ignore')
 	#consFile = "./cpp/{}_consolidate.txt".format(f_name)
-	commFile = "./python/{}_commit.txt".format(f_name)
-	issFile = "./python/{}_issue.txt".format(f_name)
+#	commFile = "./python/{}_commit.txt".format(f_name)
+#	issFile = "./python/{}_issue.txt".format(f_name)
 	pFile = "./python/{}_pullreq.txt".format(f_name)
 
 # 		# readmeFile = open(readme_name,"w")
 	#consolidateFile = open(consFile,'w',encoding="utf-8-sig", errors='ignore')
-	commitFile = open(commFile,'a',encoding="utf-8-sig", errors='ignore')
-	issueFile = open(issFile,'a',encoding="utf-8-sig", errors='ignore')
+#	commitFile = open(commFile,'a',encoding="utf-8-sig", errors='ignore')
+#	issueFile = open(issFile,'a',encoding="utf-8-sig", errors='ignore')
 	pullFile = open(pFile,'a',encoding="utf-8-sig", errors='ignore')
 	new = str(parent)+"/GHdoc/Results_all/python/"+str(file)
 
@@ -61,92 +61,92 @@ for file in f_list:
 							pullFile.write(body)
 
 
-	if(file.endswith("IRData.txt")):
+#	if(file.endswith("IRData.txt")):
 	#			consolidateFile.write("IR Data==================================")
 
-				f = open(new,encoding="utf-8-sig", errors='ignore')
-				text = f.readlines()
-				if(len(text)>1):
-					for i in range(len(text)):
-						
-						if(text[i].find("title=")!=-1):
-							title = text[i].split("=")[1]
+#				f = open(new,encoding="utf-8-sig", errors='ignore')
+#				text = f.readlines()
+#				if(len(text)>1):
+#					for i in range(len(text)):
+#						
+#						if(text[i].find("title=")!=-1):
+#							title = text[i].split("=")[1]
 	#						consolidateFile.write(title)
 							# allconsFile.write(title)
-							issueFile.write(title)
+#							issueFile.write(title)
 						
-						if(text[i].find("body =")!=-1):
-							j = i+1
+#						if(text[i].find("body =")!=-1):
+#							j = i+1
 							# print("________"+str(text[i]))
-							body = text[i].split("=")[1]
-							while(text[j].find("state")==-1):
-								body = body+(text[j])
-								j= j+1
-							# print(body)
+#							body = text[i].split("=")[1]
+#							while(text[j].find("state")==-1):
+#								body = body+(text[j])
+#								j= j+1
+#							# print(body)
 	#						consolidateFile.write(body)
 							# allconsFile.write(body)
-							issueFile.write(body)
-						if(text[i].find("comment =")!=-1):
-							j = i+1
-							comment = text[i].split("=")[1]
-
+#							issueFile.write(body)
+#						if(text[i].find("comment =")!=-1):
+#							j = i+1
+#							comment = text[i].split("=")[1]
+#
+#						
+#							while(text[j].find("comment date")==-1):
+#								comment = comment+(text[j])
+#								j= j+1
+#							print("here")	
+#							print(text[j])	
+##							# print(comment)
+#	#						consolidateFile.write(comment)
+#							# allconsFile.write(comment)
+#							issueFile.write(comment)
+#			# consolidateFile.write("==================================")				
+#
+#	
+#	if(file.endswith("CData.txt")):
+#				# consolidateFile.write("CData==================================")
+#
+#				# # commitFile.write("hi")
+#				# # commitFile.write(new)
+#				# print(new)
+#				f = open(new,encoding="utf-8-sig", errors='ignore')
+#
+#				
+#	#			commitFile.write(str(f))
+#				text = f.readlines()
+#				if(len(text)>1):
+#
+##					for i in range(len(text)):
 						
-							while(text[j].find("comment date")==-1):
-								comment = comment+(text[j])
-								j= j+1
-							print("here")	
-							print(text[j])	
+#						if(text[i].find("message")!=-1):
+#							j = i+1
+#							if(len(text[i].split("="))>1):
+#								message = text[i].split("=")[1]
+#
+#							while(text[j].find("url")==-1):
+#								if(text[j].find("comment date")==-1 and text[j].find("comment id")==-1):
+#									message = message+(text[j])
+#								j= j+1
+#							print(message)
+#							print(str(commitFile))
+#							print("_______________________")
+#							# consolidateFile.write(message)
+#							# allconsFile.write(message)
+#							commitFile.write(message)
+#
+#
+#						if(text[i].find("comment =")!=-1 ):
+#							j = i+1
+#							comment = text[i].split("=")[1]
+#							print("-------------------")
+#							
+#						
+#							while(text[j].find("comment date")==-1 or text[j].find("date =")==-1):
+#								comment = comment+(text[j])
+#
+#								j= j+1
+#							# print(comment)
 							# print(comment)
 	#						consolidateFile.write(comment)
 							# allconsFile.write(comment)
-							issueFile.write(comment)
-			# consolidateFile.write("==================================")				
-
-	
-	if(file.endswith("CData.txt")):
-				# consolidateFile.write("CData==================================")
-
-				# # commitFile.write("hi")
-				# # commitFile.write(new)
-				# print(new)
-				f = open(new,encoding="utf-8-sig", errors='ignore')
-
-				
-	#			commitFile.write(str(f))
-				text = f.readlines()
-				if(len(text)>1):
-
-					for i in range(len(text)):
-						
-						if(text[i].find("message")!=-1):
-							j = i+1
-							if(len(text[i].split("="))>1):
-								message = text[i].split("=")[1]
-
-							while(text[j].find("url")==-1):
-								if(text[j].find("comment date")==-1 and text[j].find("comment id")==-1):
-									message = message+(text[j])
-								j= j+1
-							print(message)
-							print(str(commitFile))
-							print("_______________________")
-							# consolidateFile.write(message)
-							# allconsFile.write(message)
-							commitFile.write(message)
-
-
-						if(text[i].find("comment =")!=-1 ):
-							j = i+1
-							comment = text[i].split("=")[1]
-							print("-------------------")
-							
-						
-							while(text[j].find("comment date")==-1 or text[j].find("date =")==-1):
-								comment = comment+(text[j])
-
-								j= j+1
-							# print(comment)
-							# print(comment)
-	#						consolidateFile.write(comment)
-							# allconsFile.write(comment)
-							commitFile.write(comment)	
+#							commitFile.write(comment)	
